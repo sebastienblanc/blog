225b836d3c923d98859d8a3f93001b5eef4e8d46
N-031 <n031d@proton.me>
2022-11-07 14:07:10 +0100

    hightlight plot legends

--- js-modules/political-plot.js
+++ js-modules/political-plot.js
@@ -5,11 +5,11 @@ const defaultOptions = {
   dataURL: '/static/data/political-plot-demo1.csv',
   selector: '#plot-1',
   margin: {
-    top: 30,
+    top: 50,
     right: 50,
     bottom: 10,
     left: 50,
-    legend: 200
+    legend: 200,
   },
   width: 760,
   height: 450,
@@ -18,49 +18,186 @@ const defaultOptions = {
   avgKey: 'Gauche',
   colorScale: {
     domain: ['A1', 'A2', 'B1', 'B2'],
-    range: ['#F88', '#F70', '#88F', '#08F']
+    range: ['#F88', '#F70', '#88F', '#08F'],
   },
-  idKey: 'id'
+  axisTitleRotation: -25,
+  idKey: 'id',
 };
 
 const stretchMean = (data, axisNames, idx, translations) => {
   let dataAxisNames = [...axisNames];
   dataAxisNames.splice(idx, 1);
-  return dataAxisNames.reduce((sum, prevColumnName) => {
-    let shiftedValue = Number(data[prevColumnName]) - Number(translations[prevColumnName] || 0);
-    if (shiftedValue > 5) {
-      shiftedValue = 5;
-    } else if (shiftedValue < -5) {
-      shiftedValue = -5;
-    }
-    return sum + shiftedValue;
-  }, 0) / dataAxisNames.length;
+  return (
+    dataAxisNames.reduce((sum, prevColumnName) => {
+      let shiftedValue = Number(data[prevColumnName]) - Number(translations[prevColumnName] || 0);
+      if (shiftedValue > 5) {
+        shiftedValue = 5;
+      } else if (shiftedValue < -5) {
+        shiftedValue = -5;
+      }
+      return sum + shiftedValue;
+    }, 0) / dataAxisNames.length
+  );
+};
+
+/**
+ * build a linear scale from an axis description
+ */
+const reduceAxis = (options) => (acc, columnName) => {
+  const translation = Number(options.translations[columnName] || 0);
+  const domain = translation > 0 ? [-5 + translation, 5] : [-5, 5 + translation];
+  const shiftedOrigin = (translation * options._computed.innerHeight) / 10;
+  const range = translation > 0 ? [options._computed.innerHeight, shiftedOrigin] : [options._computed.innerHeight + shiftedOrigin, 0];
+  return {
+    [columnName]: d3
+      .scaleLinear()
+      .domain(domain)
+      // --> different axis range for each group --> .domain( [d3.extent(data, function(d) { return +d[name]; })] )
+      .range(range),
+    ...acc,
+  };
 };
 
 const highlight = (key, color) => (e, d) => {
+  const selectedColumn = d[key];
 
-  const selectedColumn = d[key]
+  const targetOpacity = 0.2
+  d3.selectAll('.line').transition().duration(200).style('stroke', 'lightgrey').style('opacity', targetOpacity);
+  d3.selectAll('.legend__dot').transition().duration(200).style('fill', 'lightgrey').style('opacity', targetOpacity);
+  d3.selectAll('.legend__text').transition().duration(200).style('stroke', 'lightgrey').style('opacity', targetOpacity);
 
-  // first every group turns grey
-  d3.selectAll(".line")
-    .transition().duration(200)
-    .style("stroke", "lightgrey")
-    .style("opacity", "0.2")
-  // Second the hovered specie takes its color
-  d3.selectAll("." + selectedColumn)
-    .transition().duration(200)
-    .style("stroke", color(selectedColumn))
-    .style("opacity", "1")
-    .style("stroke-width", "8px")
-}
+  d3.selectAll('.line.' + selectedColumn)
+    .transition()
+    .duration(200)
+    .style('stroke', color(selectedColumn))
+    .style('opacity', '1')
+    .style('stroke-width', '8px');
+
+  d3.selectAll('.legend__dot.' + selectedColumn)
+    .transition()
+    .duration(200)
+    .style('fill', color(selectedColumn))
+    .style('opacity', '1')
+
+  d3.selectAll('.legend__text.' + selectedColumn)
+    .transition()
+    .duration(200)
+    .style('stroke', color(selectedColumn))
+    .style('opacity', '1')
+};
 
 const unHighlight = (key, color) => (e, d) => {
-  d3.selectAll(".line")
-    .transition().duration(200).delay(1000)
-    .style("stroke", (d) => color(d[key]))
-    .style("opacity", "1")
-    .style("stroke-width", "2px")
-}
+  const delay = 800;
+  d3.selectAll('.line')
+    .transition()
+    .duration(200)
+    .delay(delay)
+    .style('stroke', (d) => color(d[key]))
+    .style('opacity', '1')
+    .style('stroke-width', '2px');
+
+  d3.selectAll('.legend__dot')
+    .transition()
+    .duration(200)
+    .delay(delay)
+    .style('fill', (d) => color(d[key]))
+    .style('opacity', '1')
+
+  d3.selectAll('.legend__text')
+    .transition()
+    .duration(200)
+    .delay(delay)
+    .style('stroke', (d) => color(d[key]))
+    .style('opacity', '1')
+};
+
+const drawAxis = (svg, axis, horizontalScalePoint, options) => {
+  // Draw the axis:
+  const axisSVG = svg
+    .selectAll('myAxis')
+    // For each dimension of the dataset I add a 'g' element:
+    .data(options.axis)
+    .enter()
+    .append('g')
+    .attr('class', 'axis')
+    // I translate this element to its right position on the x axis
+    // .attr('transform', (axisName) => {
+    //   return `translate(${horizontalScalePoint(axisName)},${options.translations[axisName] || 0}00)`;
+    // })
+    .attr('transform', (axisName) => {
+      return `translate(${horizontalScalePoint(axisName)})`;
+    })
+    // And I build the axis with the call function
+    .each(function (d) {
+      const ticksNbr = 10 - Math.abs(options.translations[d] || 0);
+      d3.select(this).call(d3.axisLeft().ticks(ticksNbr).scale(axis[d]));
+    });
+
+  axisSVG
+    // Add axis title
+    .append('text')
+    .attr('transform', `rotate(${options.axisTitleRotation}), translate(0, -10)`)
+    .style('text-anchor', 'middle')
+    .attr('y', -9)
+    .text((d) => d)
+    .style('fill', 'white');
+
+  return axisSVG;
+};
+
+const drawLines = (svg, axis, data, horizontalScalePoint, color, options) =>
+  svg
+    .selectAll('myPath')
+    .data(data)
+    .join('path')
+    .attr('class', (d) => 'line ' + d[options.idKey]) // 2 class for each line: 'line' and the group name
+    .attr('d', pathFromCSVRow(horizontalScalePoint, options, axis))
+    .style('fill', 'none')
+    .style('stroke', (d) => color(d[options.idKey]))
+    .style('stroke-width', '2px')
+    .style('opacity', 0.6)
+    .on('mouseover', highlight(options.idKey, color))
+    .on('mouseleave', unHighlight(options.idKey, color));
+
+const drawLegend = (svg, data, color, options) => {
+  // Add one dot in the legend for each name.
+  const legendSVG = svg
+    .append('g')
+    .attr('class', 'legend')
+    .attr('transform', `translate(${options.width - options.margin.legend},${options.margin.top})`)
+    .style('background', '#FFF');
+
+  legendSVG
+    .selectAll('mydots')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', 0)
+    .attr('cy', (d, i) => 100 + i * 25) // 100 is where the first dot appears. 25 is the distance between dots
+    .attr('r', 7)
+    .attr('class', (d) => 'legend__dot ' + d[options.idKey]) // 2 class for each line: 'line' and the group name
+    .style('fill', (d) => color(d[options.idKey]))
+    .on('mouseover', highlight(options.idKey, color))
+    .on('mouseleave', unHighlight(options.idKey, color));
+
+  // Add one dot in the legend for each name.
+  legendSVG
+    .selectAll('mylabels')
+    .data(data)
+    .enter()
+    .append('text')
+    .attr('class', (d) => 'legend__text ' + d[options.idKey]) // 2 class for each line: 'line' and the group name
+    .attr('x', 20)
+    .attr('y', (d, i) => 100 + i * 25) // 100 is where the first dot appears. 25 is the distance between dots
+    .style('fill', (d) => color(d[options.idKey]))
+    .text((d) => d[options.idKey])
+    .attr('text-anchor', 'left')
+    .style('alignment-baseline', 'middle')
+    .on('mouseover', highlight(options.idKey, color))
+    .on('mouseleave', unHighlight(options.idKey, color));
+
+  return legendSVG;
+};
 
 const pathFromCSVRow =
   (horizontalScalePoint, options, axis) =>
@@ -100,22 +237,21 @@ const pathFromCSVRow =
 
 export async function drawPoliticalPlot(opts) {
   const options = { ...defaultOptions, ...opts };
-  // set the dimensions and margins of the graph
-  const contentWidth = options.width - options.margin.left - options.margin.right;
-  const innerWidth = options.legend ? contentWidth - options.margin.legend : contentWidth;
-  const innerHeight = options.height - options.margin.top - options.margin.bottom;
-  const { margin, axis: axisNames } = options;
+  {
+    const contentWidth = options.width - options.margin.left - options.margin.right;
+    options._computed = {
+      contentWidth: options.width - options.margin.left - options.margin.right,
+      innerWidth: options.legend ? contentWidth - options.margin.legend : contentWidth,
+      innerHeight: options.height - options.margin.top - options.margin.bottom,
+    };
+  }
 
   // append the svg object to the body of the page
-  const svg = d3
-    .select(options.selector)
-    .append('svg')
-    .attr('width', options.width)
-    .attr('height', options.height);
-  
+  const svg = d3.select(options.selector).append('svg').attr('width', options.width).attr('height', options.height);
+
   const innerSVG = svg
     .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`)
+    .attr('transform', `translate(${options.margin.left},${options.margin.top})`)
     .style('background', '#1b1e23')
     .style('color', '#fff');
 
@@ -125,94 +261,16 @@ export async function drawPoliticalPlot(opts) {
   // Color scale: give me a specie name, I return a color
   const color = d3.scaleOrdinal().domain(options.colorScale.domain).range(options.colorScale.range);
 
-  // For each dimension, I build a linear scale. I store all in a y object
-  const axis = axisNames.reduce((acc, columnName) => {
-    const translation = Number(options.translations[columnName] || 0);
-    const domain = translation > 0 ? [-5 + translation, 5] : [-5, 5 + translation];
-    const shiftedOrigin = (translation * innerHeight) / 10;
-    const range = translation > 0 ? [innerHeight, shiftedOrigin] : [innerHeight + shiftedOrigin, 0];
-    return {
-      [columnName]: d3
-        .scaleLinear()
-        .domain(domain)
-        // --> different axis range for each group --> .domain( [d3.extent(data, function(d) { return +d[name]; })] )
-        .range(range),
-      ...acc,
-    };
-  }, {});
-
   // Build the X scale -> it find the best position for each Y axis
-  const horizontalScalePoint = d3.scalePoint().range([0, innerWidth]).domain(axisNames);
+  const horizontalScalePoint = d3.scalePoint().range([0, options._computed.innerWidth]).domain(options.axis);
 
-  // Draw the lines
-  innerSVG
-    .selectAll('myPath')
-    .data(data)
-    .join('path')
-    .attr('class', (d) => 'line ' + d[options.idKey]) // 2 class for each line: 'line' and the group name
-    .attr('d', pathFromCSVRow(horizontalScalePoint, options, axis))
-    .style('fill', 'none')
-    .style('stroke', (d) => color(d[options.idKey]))
-    .style('stroke-width', "2px")
-    .style('opacity', 0.6)
-    .on("mouseover", highlight(options.idKey, color))
-    .on("mouseleave", unHighlight(options.idKey, color));
+  const axis = options.axis.reduce(reduceAxis(options), {});
 
-  // Draw the axis:
-  innerSVG
-    .selectAll('myAxis')
-    // For each dimension of the dataset I add a 'g' element:
-    .data(axisNames)
-    .enter()
-    .append('g')
-    .attr('class', 'axis')
-    // I translate this element to its right position on the x axis
-    // .attr('transform', (axisName) => {
-    //   return `translate(${horizontalScalePoint(axisName)},${options.translations[axisName] || 0}00)`;
-    // })
-    .attr('transform', (axisName) => {
-      return `translate(${horizontalScalePoint(axisName)})`;
-    })
-    // And I build the axis with the call function
-    .each(function (d) {
-      const ticksNbr = 10 - Math.abs(options.translations[d] || 0);
-      d3.select(this).call(d3.axisLeft().ticks(ticksNbr).scale(axis[d]));
-    })
-    // Add axis title
-    .append('text')
-    .style('text-anchor', 'middle')
-    .attr('y', -9)
-    .text((d) => d)
-    .style('fill', 'white');
+  drawLines(innerSVG, axis, data, horizontalScalePoint, color, options);
+
+  drawAxis(innerSVG, axis, horizontalScalePoint, options);
 
   if (options.legend) {
-    // Add one dot in the legend for each name.
-    const legendSVG = svg.append('g')
-      .attr('class', 'legend')
-      .attr('transform', `translate(${options.width - options.margin.legend},${margin.top})`)
-      .style('background', "#FFF")
-    
-    legendSVG
-      .selectAll("mydots")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("cx", 0)
-      .attr("cy", (d,i) => 100 + i*25) // 100 is where the first dot appears. 25 is the distance between dots
-      .attr("r", 7)
-      .style("fill", (d) => color(d[options.idKey]))
-    
-    // Add one dot in the legend for each name.
-    legendSVG
-      .selectAll("mylabels")
-      .data(data)
-      .enter()
-      .append("text")
-      .attr("x", 20)
-      .attr("y", (d,i) => 100 + i*25) // 100 is where the first dot appears. 25 is the distance between dots
-      .style("fill", (d) => color(d[options.idKey]))
-      .text((d) => d[options.idKey])
-      .attr("text-anchor", "left")
-      .style("alignment-baseline", "middle")
+    drawLegend(svg, data, color, options);
   }
 }
